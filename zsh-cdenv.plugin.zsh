fd2af331f23fd25e60e79a6ebab64e1d84ddcd7f
Javier Luna molina <javierlunamolina@gmail.com>
2020-09-05 17:44:06 +0200

    Fix plugin deactivating environments when on not-git-repository folders. Fix plugin not updating the virtual environment when changing between git repositories

--- zsh-cdenv.plugin.zsh
+++ zsh-cdenv.plugin.zsh
@@ -1,32 +1,41 @@
 # Auto activate/deactivate virtual environments if a git repo is detected.
 # Im no POSIX expert sorry
 
-function cd {
-    builtin cd "$@"
+function _zsh_cdenv_current_dir {
+	git rev-parse --show-toplevel 2> /dev/null
+}
+
+function _zsh_cdenv_activate_venv {
     if [ ! -z "$POSSIBLE_ENV_NAMES" ]; then
         POSSIBLE_ENV_NAMES=( "env" "ENV" "venv" "VENV" ) # In a future version this could be customizable
     fi
-    REPO=$(git rev-parse --show-toplevel 2> /dev/null)
-    IS_IN_REPO=$?
 
+    for ENV_NAME in $POSSIBLE_ENV_NAMES; do
+	if [ -e "$1/$ENV_NAME/bin/activate" ]; then
+		source $1/$ENV_NAME/bin/activate
+                break
+        fi
+    done
+}
+
+function cd {
+    builtin cd "$@"
+
+    local CURRENT_REPOSITORY=$(_zsh_cdenv_current_dir)
     if [ ! -z "$VIRTUAL_ENV" ]; then
         IS_IN_VIRTUAL_ENV=1
     else
         IS_IN_VIRTUAL_ENV=0
     fi
-    if [ $IS_IN_REPO -eq 0 ]; then
+
+    if [[ "$_ZSH_CDENV_LAST_REPOSITORY" != "$CURRENT_REPOSITORY" ]]; then
         if [ $IS_IN_VIRTUAL_ENV -eq 0 ]; then
-            for ENV_NAME in $POSSIBLE_ENV_NAMES; do
-                if [ -e "$REPO/$ENV_NAME/bin/activate" ]; then
-                    source $REPO/$ENV_NAME/bin/activate
-                    break
-                fi
-            done
-        fi
-    else
-        if [ $IS_IN_VIRTUAL_ENV -eq 1 ]; then
-            deactivate
-        fi
+        	_zsh_cdenv_activate_venv $CURRENT_REPOSITORY    
+	else
+	    deactivate
+	    _zsh_cdenv_activate_venv $CURRENT_REPOSITORY
+    	fi
     fi
+    export _ZSH_CDENV_LAST_REPOSITORY=$(_zsh_cdenv_current_dir)
 }
 
